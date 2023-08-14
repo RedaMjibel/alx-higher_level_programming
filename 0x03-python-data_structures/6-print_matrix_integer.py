f02707fc14da40a380036ff33e73bf072209c430
redamjibel <redamjibel@hotmail.com>
2023-08-14 13:48:22 -0700

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for inside_list in matrix:
         for number in inside_list:
-            print("{:d}".format(number), end=" "if number != inside_list[-1] else "")
+            print("{:d}".format(number),
+                  end=" "if number != inside_list[-1] else "")
         print()
