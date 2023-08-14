10972b52771b46b7235158cf041f32d068283cfe
redamjibel <redamjibel@hotmail.com>
2023-08-14 13:41:48 -0700

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for inside_list in matrix:
         for number in inside_list:
-            print("{:d}".format(number), end=" ")
+            print("{}".format(number), end=" ")
         print()
