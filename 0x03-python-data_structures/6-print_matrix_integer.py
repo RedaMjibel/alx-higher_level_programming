8eb0a0c57417a846074f3040e8bafe94e0fe4a67
redamjibel <redamjibel@hotmail.com>
2023-08-14 13:34:35 -0700

    itslikeatwodarrayinc

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for inside_list in matrix:
+        for number in inside_list:
+            print("{:d}".format(number), end=" ")
+        print()
