833c2cd358e4edfa76f58d8fbb64ea4b31010b16
redamjibel <redamjibel@hotmail.com>
2023-08-15 19:01:54 -0700

    updatebutsegfault

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,6 +10,42 @@
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || head->next == NULL)
+	struct listint_s *slow = *head;
+	struct listint_s *fast = *head;
+	struct listint_s *rev = NULL;
+	struct listint_s *temp = NULL;
+
+	if ((*head) == NULL || (*head)->next == NULL)
 		return 1;
+	while (1)
+	{
+		fast = fast->next->next;
+		if (fast->next == NULL)
+		{
+			slow = slow->next->next;
+			break;
+		}
+		if (fast == NULL)
+		{
+			slow = slow->next;
+			break;
+		}
+		slow = slow->next;
+	}
+	slow->next = NULL;
+	while (slow != NULL)
+	{
+		temp = slow->next;
+		slow->next = rev;
+		rev = slow;
+		slow = temp;
+	}
+	while (head != NULL && rev != NULL)
+	{
+		if ((*head)->n != rev->n)
+			return (0);
+		*head = (*head)->next;
+		rev = rev->next;
+	}
+	return (1);
 }
