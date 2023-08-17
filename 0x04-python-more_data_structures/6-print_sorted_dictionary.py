6895470d93ea26613279fea390417b2e79de64de
redamjibel <redamjibel@hotmail.com>
2023-08-16 18:20:56 -0700

    justusesortediguess

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,4 @@
+def print_sorted_dictionary(a_dictionary):
+    sorted_dictionary = sorted(a_dictionary.keys())
+    for key in sorted_dictionary:
+        print("{}: {}".format(key, a_dictionary[key]))
