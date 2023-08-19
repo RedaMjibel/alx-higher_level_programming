cf893b63e310979fcae03ac8b229db50620e30ba
redamjibel <redamjibel@hotmail.com>
2023-08-18 17:42:17 -0700

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+            'D': 500, 'M':1000 }
+    total = 0
+    prev = 0
+
+    for num in reversed(roman_string):
+        integer = roman_numerals[num]
+        if integer <= prev:
+            total = total - integer
+        elif integer >= prev:
+            total = total + integer
+
+    return total
