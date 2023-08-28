55bd00ea38ef74c482ea9bf603a544e1d4727d8a
redamjibel <redamjibel@hotmail.com>
2023-08-28 14:43:48 -0700

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             if x > 0 and i >= x:
                 break
         except Exception as ex:
-            print(f"Error: {ex}")
+            return i
     print(" ")
     return i
