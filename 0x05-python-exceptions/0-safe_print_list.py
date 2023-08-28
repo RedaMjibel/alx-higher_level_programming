26ce48ee22b846451de44debcf8efe3c62126da9
redamjibel <redamjibel@hotmail.com>
2023-08-28 14:46:18 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             i = i + 1
             if x > 0 and i >= x:
                 break
-        except Exception as ex:
-            return i
+        except IndexError:
+            None
     print(" ")
     return i
