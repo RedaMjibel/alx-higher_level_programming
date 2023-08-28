a6813f4f3671218ac519ab2050c921e37390f69c
redamjibel <redamjibel@hotmail.com>
2023-08-28 14:53:25 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         while i is not x:
             print(my_list[i], end='')
-            i = i + 1
+            i += 1
     except IndexError:
         None
-    print(" ")
+    print()
     return i
