53f59930acfd5a2c657cae8282f3b8a33721d424
redamjibel <redamjibel@hotmail.com>
2023-08-29 16:52:23 -0700

    indent

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,9 @@
 class Square:
     """square class"""
     def __init__(self, size=0):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        """instanitation"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
