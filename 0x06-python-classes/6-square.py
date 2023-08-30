d829ce425a98fa5f121eaae76d60c3d30fc4be0c
redamjibel <redamjibel@hotmail.com>
2023-08-29 18:42:26 -0700

    test

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class Square:
+    """This is a class"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """property to retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """method"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """print method"""
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 and i != j else "")
+        print()
