fb758c526a1ba17c60decab1eb1bc378fbd1192f
redamjibel <redamjibel@hotmail.com>
2023-09-04 13:17:56 -0700

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,6 +5,10 @@
 class Rectangle:
     """This is a rectangle"""
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         if height < 0:
