0681cd77b50d29ef922c48a39a22b8cec08b3a59
redamjibel <redamjibel@hotmail.com>
2023-09-04 12:50:41 -0700

    private

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """This is a rectangle"""
     def __init__(self, width=0, height=0):
         self.__width = width
-        self.height = height
+        self.__height = height
 
     @property
     def width(self):
