c4b55c49ea3a2273e54262a3b7c2f0cd33eebe77
redamjibel <redamjibel@hotmail.com>
2023-09-04 13:01:53 -0700

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,6 +5,10 @@
 class Rectangle:
     """This is a rectangle"""
     def __init__(self, width=0, height=0):
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
