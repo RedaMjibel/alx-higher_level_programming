5659230ee892176af9ed0ce712b49d174d0754bf
redamjibel <redamjibel@hotmail.com>
2023-09-11 19:07:38 -0700

    test

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+def is_same_class(obj, a_class):
+    """Determines if an object is an instance of a class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
