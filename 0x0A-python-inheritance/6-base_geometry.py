8ee92f8deda5bbed52b1e6311ac0c17d2745bfb0
redamjibel <redamjibel@hotmail.com>
2023-09-11 19:49:48 -0700

    raise

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
