3e1927604df0e592b14b847134e29aeca4b9dc08
redamjibel <redamjibel@hotmail.com>
2023-09-13 15:12:38 -0700

    test

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Public instance method that validates value"""
+        if isinstance(value, int):
+            pass
+        else:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
