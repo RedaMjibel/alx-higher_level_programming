5e460d02dc3498cc38ace84802fc31f235a4fb83
redamjibel <redamjibel@hotmail.com>
2023-09-13 18:49:16 -0700

    test

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        for attr in attrs:
+            if type(attr) is not str:
+                return self.__dict__
+        thisdict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                thisdict[key] = value
+        return thisdict
