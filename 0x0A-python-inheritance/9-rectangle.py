0ed4ce423bec044855b3312c3274bc27f02195db
redamjibel <redamjibel@hotmail.com>
2023-09-13 17:15:33 -0700

    test

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Documentation"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class rectangle that inherits from BaseGeometry class"""
+    def __init__(self, width, height):
+        """construct"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def area(self):
+        """return the area of a rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
