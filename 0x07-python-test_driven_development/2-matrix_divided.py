9f378f3e4de287fc7e9d09e5f62e56d9e2126065
redamjibel <redamjibel@hotmail.com>
2023-09-28 10:33:02 -0700

    test

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements in a matrix"""
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
+    for row in matrix:
+        if not isinstance(row, list) or len(row) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for n in row:
+            if not isinstance(n, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                                "of integers/floats")
+    result = [[0 for _ in row] for _ in range(len(matrix))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[i][j] = round(matrix[i][j] / div, 2)
+    return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
