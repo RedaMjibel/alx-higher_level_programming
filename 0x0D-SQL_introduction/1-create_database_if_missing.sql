a5a64bff9e9ff5e49330295ec4abce3ab47dde41
redamjibel <redamjibel@hotmail.com>
2023-10-17 17:55:25 -0700

    did it really not work just because of a space

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,5 @@
---creates the database hbtn_0c_0
---in this SQL server
+-- 1-create_database_if_missing.sql
+
+-- attempts to create the database if it doesn't exist
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
