ba4d771883345da935a5b29f7db20d37e843cbc0
redamjibel <redamjibel@hotmail.com>
2023-10-18 18:08:37 -0700

    auto increment

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- 5-unique_id.sql
+-- creates the table id_not_null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);
