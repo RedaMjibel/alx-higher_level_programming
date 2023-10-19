2cc8c4443d23e0cca4c142a72f3a36b5ab707507
redamjibel <redamjibel@hotmail.com>
2023-10-19 15:24:23 -0700

    test

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- 16-shows_by_genre.sql
+-- lists all shows, and all genres linked to that show
+
+SELECT ts.title, tg.name
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg on tsg.genre_id = tg.id
+ORDER BY ts.title ASC, tg.name ASC;
