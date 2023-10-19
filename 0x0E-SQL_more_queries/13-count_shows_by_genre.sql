d9c0dec6585f4c108fdc84493390792c8ee63cbc
redamjibel <redamjibel@hotmail.com>
2023-10-18 19:57:15 -0700

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 
 SELECT tg.name AS genre, COUNT(tsg.show_id) AS number_of_shows
 FROM tv_genres tg
-LEFT JOIN tv_showgenres tsg ON tg.id = tsg.genre_id
+LEFT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 GROUP BY tg.name
 HAVING COUNT(tsg.show_id) > 0
 ORDER BY number_of_shows DESC;
