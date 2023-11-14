5cbd0600eeb4946d4ad3ee08e59acb32b45b8b53
redamjibel <redamjibel@hotmail.com>
2023-11-13 17:50:21 -0800

    repeat

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const check = process.argv[2];
+
+if (isNaN(check) || check === undefined) {
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  for (let i = 0; i < x; i++) {
+	console.log('X'.repeat(x));
+  }
+}
