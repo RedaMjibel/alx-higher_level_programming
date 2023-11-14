88a843e57d7e881c7a9949934a62c705391d736a
redamjibel <redamjibel@hotmail.com>
2023-11-13 18:45:11 -0800

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const array = process.argv.slice(2).map(Number);
-  const second = array.sort(function (a, b) { return (b - a); }) [1];
+  const second = array.sort(function (a, b) { return (b - a); })[1];
   console.log(second);
 }
