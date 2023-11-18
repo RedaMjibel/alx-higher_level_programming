94feb61cb794eaa455cd9f374711b73f2d3f7403
redamjibel <redamjibel@hotmail.com>
2023-11-18 14:53:27 -0800

    only N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" lists all states in the database hbtn_0e_0_usa starting with 'N' """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
+
+    Cursor = db.cursor()
+    Cursor.execute("SELECT * FROM states WHERE name\
+                   LIKE BINARY 'N%' ORDER BY id ASC")
+
+    rows = Cursor.fetchall()
+    for i in rows:
+        print(i)
+    Cursor.close()
+    db.close()
