4da032d6fc677187b129d2824bc179bf2d82138c
redamjibel <redamjibel@hotmail.com>
2023-11-17 18:21:08 -0800

    sql not working test anyways

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
+
+    Cursor = db.cursor()
+    Cursor.execute("SELECT * FROM states")
+
+    rows = Cursor.fetchall()
+    for i in rows:
+        print(i)
+    Cursor.close()
+    db.close()
