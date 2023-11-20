0926c47726f5bae2049022dd25d04c446d192e83
redamjibel <redamjibel@hotmail.com>
2023-11-19 17:30:08 -0800

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-contains the class definition of a city 
+contains the class definition of a city
 
 """
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
