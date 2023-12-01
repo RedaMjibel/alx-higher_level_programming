ed6fc5c67a69be3b8efb045853b97371b4ab3aae
redamjibel <redamjibel@hotmail.com>
2023-12-01 14:00:57 -0800

    fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" fetches a URL an prints the body of the response """
+
+
+import urllib.request
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type:{}".format(type(content)))
+            print("\t- content:{}".format(content))
+            print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    except urllib.error.URLError as e:
+        print(f"Error:{e}")
