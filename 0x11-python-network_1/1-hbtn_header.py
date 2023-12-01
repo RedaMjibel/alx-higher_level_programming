5e62ca33eab671f236c66bf18b5dec28f5206dcd
redamjibel <redamjibel@hotmail.com>
2023-12-01 14:37:18 -0800

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,14 +3,15 @@
 
 
 import urllib.request
+import sys
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
 
     try:
-        with urllib.request.urlopen(url) as response:
-            x_request_id = response.getheader('X-Request-Id')
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            x_request_id = response.info()
             if x_request_id:
-                print(x_request_id)
+                print(x_request_id['X-Request-Id'])
     except urllib.error.URLError as e:
         print(f"Error:{e}")
