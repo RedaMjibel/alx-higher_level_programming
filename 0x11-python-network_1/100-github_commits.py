720b8e4da2f1a78d52ff1b8b32b7e41cd6770012
redamjibel <reda_mjibel@hotmail.com>
2023-12-01 18:38:05 -0800

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-""" fetches 10 commits from a repository """
-
+"""  fetches commits from a githun repo  """
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    response = requests.get('https://api.github.com/repos/{}/{}/commits'
-                            .format(argv[1], argv[2]))
     i = 0
+    response = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
     for commit in sorted(response.json(), key=lambda c: c.get('commit')
                          .get('author').get('date'), reverse=True):
         print(commit.get('sha') + ": ", end="")
         print(commit.get('commit').get('author').get('name'))
-        i = i + 1
+        i += 1
         if i == 10:
             break
