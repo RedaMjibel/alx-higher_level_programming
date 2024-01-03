31814c921fa7168203cd8ed3dacc39ebfbef5215
RedaMjibel <reda_mjibel@hotmail.com>
2024-01-03 15:48:10 -0800

    readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('node:fs');
+
+const path = process.argv[2];
+
+fs.readFile(path, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
