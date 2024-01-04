d224936f82fb9a2d5f185be68221929370c5f1da
RedaMjibel <reda_mjibel@hotmail.com>
2024-01-03 18:03:44 -0800

    update

--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
 let count = 0;
-request(url, function (error, response, body) {
+const url = process.argv[2];
+
+request(url, function (error, respones, body) {
   if (error) {
     console.log(error);
   } else {
     const info = JSON.parse(body);
-    const info2 = info.results;
-    for (let i = 0; i < info2.length; i++) {
-      if (info2[i].characters) {
-        for (const j of info2[i].characters) {
+    const infres = info.results;
+    for (let i = 0; i < infores.length; i++) {
+      if (infres[i].characters) {
+        for (const j of infres[i].characters) {
           if (j.includes('18')) {
             count++;
           }
