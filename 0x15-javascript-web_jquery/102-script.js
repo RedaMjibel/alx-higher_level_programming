6c06845830a81ee3734c5e26f3e7272dbbf89889
redamjibel <redamjibel@hotmail.com>
2024-01-11 12:36:23 -0800

    update

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
   $('INPUT#btn_translate').click(function () {
-    const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+    const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
     const lang = $('INPUT#language_code').val();
     const request = url + lang;
     $.getJSON(request, function (data) {
